b67d28c9da538b7258707c08625196a237a7624e
Valentina Valverde <vvgreenville@gmail.com>
2023-10-02 13:19:55 -0400

    Got through part of the tutorial in class.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Root, { loader as rootLoader } from "./route/root.jsx";
+import ErrorPage from "./route/errorPage.jsx";
+import Contact from "./route/contacts.jsx"
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
